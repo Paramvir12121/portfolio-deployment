7c314c4b073ee3a0d8f6741b0959df17871d6807
Paramvir Singh <paramvir12121@gmail.com>
2023-12-02 21:16:22 +0000

    added route53

---
+++ terraform/modules/route53/variable.tf
@@ -0,0 +1,16 @@
+
+resource "aws_route53_zone" "zone" {
+  name = "iamparamvirsingh.com"
+}
+
+resource "aws_route53_record" "s3site" {
+  zone_id = aws_route53_zone.zone.zone_id
+  name = "iamparamvirsingh.com"
+  type = "A"
+  
+  alias {
+    name = "s3-website.us-east-1.amazonaws.com"
+    zone_id = aws_route53_zone.zone.zone_id
+    evaluate_target_health = false
+  }
+}
