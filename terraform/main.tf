25c5681d0ca90c9e084fe2e4bc2cba86e37960f3
paramvir12121 <paramvir12121@gmail.com>
2023-12-04 21:33:06 -0500

    linkedin added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
 module "s3_bucket_webpages" {
-  source          = "./modules/s3_bucket_webpages"
-  aws_region      = local.aws_region
+  source     = "./modules/s3_bucket_webpages"
+  aws_region = local.aws_region
   # List of files to upload
-  files_to_upload = ["index.html", "styles.css", "scripts.js", "assets/img/profile.jpg"] 
-  bucket_name               = "param-portfolio-1.0.12"
+  files_to_upload           = ["index.html", "styles.css", "scripts.js", "assets/img/profile.jpg"]
+  bucket_name               = "iamparamvirsingh.com"
   aws_credentials_file_path = var.aws_credentials_file_path
   index_document            = "index.html"
   tags                      = local.tags
 }
+
 
 
